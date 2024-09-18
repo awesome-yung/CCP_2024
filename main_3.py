a8f164685cf18bdc5ba3349602822431c136bbfb
yumin <yung.ym.lee@gmail.com>
2024-09-18 19:19:33 +0900

    Update main

--- main_3.py
+++ main_3.py
@@ -38,7 +38,7 @@ LR = 1e-5
 NUM_EPOCHS = 5
 VAL_EVERY = 1
 file_name = 'flash.pt'
-train_batch_size = 32
+train_batch_size = 16
 valid_batch_size = 4
 
 train_loader, valid_loader = get_dataloader(root ='./ADE20K/ADEChallengeData2016', train_batch_size=train_batch_size, valid_batch_size=valid_batch_size)
